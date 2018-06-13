bef9be649d689e2e4501a3c4ee8bedb49d48b283
jenjenmuj <fr.konecny@gmail.com>
2018-06-13 21:38:23 +0200

    mapy hotove, detail hotovy

--- app/src/main/java/cz/uhk/fim/umte/myvespa/ViewPlace.java
+++ app/src/main/java/cz/uhk/fim/umte/myvespa/ViewPlace.java
@@ -1,5 +1,7 @@
 package cz.uhk.fim.umte.myvespa;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -26,6 +28,8 @@ public class ViewPlace extends AppCompatActivity {
 
     IGoogleAPIService mService;
 
+    PlaceDetail mPlace;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +44,28 @@ public class ViewPlace extends AppCompatActivity {
         place_name = (TextView) findViewById(R.id.place_name);
         btnViewOnMap = (Button) findViewById(R.id.btn_show_map);
 
+        // Empty all views
+        place_name.setText("");
+        place_address.setText("");
+        opening_hours.setText("");
+
+
+        // Set button to fetch url from Place Detail to go to GMaps
+        btnViewOnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mPlace.getResult().getUrl()));
+                startActivity(mapIntent);
+            }
+        });
 
         // Photos - Check if photo is not null
         if (Common.currentResult.getPhotos() != null && Common.currentResult.getPhotos().length > 0) {
             // getPhotos is array, we will use onlz first image
             Picasso.with(this)
                     .load(getPhotoOfPlace(Common.currentResult.getPhotos()[0].getPhoto_reference(), 1000))
+                    .placeholder(R.drawable.ic_image_black_24dp)
+                    .error(R.drawable.ic_error_black_24dp)
                     .into(photo);
         }
 
@@ -58,12 +78,36 @@ public class ViewPlace extends AppCompatActivity {
 
         //Opening hours
         if (Common.currentResult.getOpening_hours() != null) {
-            opening_hours.setText("Open now : "Common.currentResult.getOpening_hours().getOpen_now());
+            opening_hours.setText("Open now : " + Common.currentResult.getOpening_hours().getOpen_now());
         } else {
             opening_hours.setVisibility(View.GONE);
         }
 
+        // User service to fetch Address nad Name
+        mService.getDetail(getPlaceDetailUrl(Common.currentResult.getPlace_id()))
+                .enqueue(new Callback<PlaceDetail>() {
+                    @Override
+                    public void onResponse(Call<PlaceDetail> call, Response<PlaceDetail> response) {
+                        mPlace = response.body();
 
+                        place_address.setText(mPlace.getResult().getFormatted_address());
+                        place_name.setText(mPlace.getResult().getName());
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<PlaceDetail> call, Throwable t) {
+
+                    }
+                });
+
+    }
+
+    private String getPlaceDetailUrl(String place_id) {
+        StringBuilder googlePlaceDetailUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/details/json?")
+                .append("place_id=" + place_id)
+                .append("&key=" + getResources().getString(R.string.browser_key));
+        return googlePlaceDetailUrl.toString();
     }
 
     private String getPhotoOfPlace(String photo_reference, int maxWidth) {
